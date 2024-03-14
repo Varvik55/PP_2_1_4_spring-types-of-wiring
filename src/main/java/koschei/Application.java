78954e3051e96c72d2c506985718cd4b3c6029e0
gribovkirill <qwertyastilark@mail.ru>
2024-03-14 10:09:53 +0400

    Commin1

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -10,6 +10,7 @@ public class Application {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
 
-@Component
+//Связывание через Been
 public class Duck5 {
+    Egg6 egg6;
+
+    public Duck5(Egg6 egg6) {
+        this.egg6 = egg6;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg6.toString();
     }
 }
